fe2b6311b5a5e112a3c54aa047d25737e431aaa9
aierou <aierou@live.com>
2016-10-05 20:41:54 -0400

    Slight refactoring of error module

--- error.js
+++ error.js
@@ -1,6 +1,6 @@
 var exports = module.exports;
 
-var ERROR_API=1;
+const ERROR_API = 1;
 
 exports.handler = function(err, req, res, next){
   if(err.type==ERROR_API){
@@ -11,30 +11,20 @@ exports.handler = function(err, req, res, next){
   }
 }
 
-exports.BadRequest = function(message, errorCode){
-  this.message = message || "Bad request.";
-  this.statusCode = 400;
-  this.errorCode = errorCode || 400;
-  this.type = ERROR_API;
+ErrorFactory = function(){
+  this.getAPIError = function(statusCode, default_message){
+    return function(message, errorCode){
+      this.message = message || default_message;
+      this.statusCode = statusCode;
+      this.errorCode = errorCode || statusCode;
+      this.type = ERROR_API;
+    }
+  }
 }
 
-exports.Forbidden = function(message, errorCode){
-  this.message = message || "Permissions not met for the request.";
-  this.statusCode = 403;
-  this.errorCode = errorCode || 403;
-  this.type = ERROR_API;
-}
+errorFactory = new ErrorFactory();
 
-exports.NotFound = function(message, errorCode){
-  this.message = message || "The requested resource couldn't be found.";
-  this.statusCode = 404;
-  this.errorCode = errorCode || 404;
-  this.type = ERROR_API;
-}
-
-exports.Conflict = function(message, errorCode){
-  this.message = message || "There was a conflict with the request.";
-  this.statusCode = 409;
-  this.errorCode = errorCode || 409;
-  this.type = ERROR_API;
-}
+exports.BadRequest = errorFactory.getAPIError(400, "Bad request.");
+exports.Forbidden = errorFactory.getAPIError(403, "Permissions not met for the request.");
+exports.NotFound = errorFactory.getAPIError(404, "The requested resource couldn't be found.");
+exports.Conflict = errorFactory.getAPIError(409, "There was a conflict with the request.");
