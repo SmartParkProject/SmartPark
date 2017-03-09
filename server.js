e7441896f08b0e5f34da7b6269a9147e64606c66
aierou <aierou@live.com>
2017-03-08 19:35:36 -0500

    Small fix

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ if(true || process.env.NODE_ENV == "development"){
 }
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
   next();
 });
 
