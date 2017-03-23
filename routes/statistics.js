9093d88a3fbcc2b812fe0008ccf4681009c71f5f
aierou <aierou@live.com>
2017-03-23 17:27:33 -0400

    Add response to traffic statistics handler

--- routes/statistics.js
+++ routes/statistics.js
@@ -5,6 +5,8 @@ var router = express.Router();
 router.post("/traffic", function(req, res, next){
   var data = req.body;
   console.log(JSON.stringify(data));
+  res.status(200);
+  res.json({status:200});
 });
 
 module.exports = router;
