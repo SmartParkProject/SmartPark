1b452762d8cca1bfd2f95de9b303a2de29845474
aierou <aierou@live.com>
2017-03-28 16:25:59 -0400

    Delete old web content

--- public/files/scripts/logout.js
+++
@@ -1,8 +0,0 @@
-function display_warning(){
-	$("#warnModal").modal('show');
-}
-
-function crack_token(){
-	var old_cookie = document.cookie;
-	document.cookie = old_cookie + " expires=Thu, 01 Jan 1970 00:00:01 GMT;";
-}
