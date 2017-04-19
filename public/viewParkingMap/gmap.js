5c0acbfd3964c2a14f36535785c8ea61d5823b91
Blackale11 <aely@umflint.edu>
2017-04-18 23:17:12 -0400

    Changes to documents
    Added error handling for map/location functions, added a hidden menu in accountman for admin/privledged users

--- public/viewParkingMap/gmap.js
+++ public/viewParkingMap/gmap.js
@@ -1,15 +1,18 @@
 function initMap() {
 	var map;
 	if (navigator.geolocation) {
-        var position = navigator.geolocation.getCurrentPosition(showPosition);
+        var position = navigator.geolocation.getCurrentPosition(showPosition, postError);
 	}
 	else{
+
 	  map = new google.maps.Map(document.getElementById("map"), {
-          center: {lat: 63.0199770, lng: -83.6876530},
+          center: {lat: 43.0199770, lng: -83.6876530},
           zoom: 15
         });
+	  getMapLocError(43.0199770, -83.6876530);
 	}
       }
+
 function showPosition(position) {
 	var map;
 	map = new google.maps.Map(document.getElementById("map"), {
@@ -17,6 +20,43 @@ function showPosition(position) {
           zoom: 15
 });
 	getMapLoc(position);
+}
+function postError(error){
+	var map;
+	switch(error.code) {
+        case error.PERMISSION_DENIED:
+            console.log("User denied the request for Geolocation.");
+	    map = new google.maps.Map(document.getElementById("map"), {
+            center: {lat: 43.0199770, lng: -83.6876530},
+            zoom: 15
+            });
+		getMapLocError(43.0199770, -83.6876530);
+            break;
+        case error.POSITION_UNAVAILABLE:
+            console.log("Location information is unavailable.");
+            map = new google.maps.Map(document.getElementById("map"), {
+            center: {lat: 43.0199770, lng: -83.6876530},
+            zoom: 15
+            });
+		getMapLocError(43.0199770, -83.6876530);
+            break;
+        case error.TIMEOUT:
+            console.log("The request to get user location timed out.");
+            map = new google.maps.Map(document.getElementById("map"), {
+            center: {lat: 43.0199770, lng: -83.6876530},
+            zoom: 15
+            });
+		getMapLocError(43.0199770, -83.6876530);
+            break;
+        case error.UNKNOWN_ERROR:
+            console.log("An unknown error occurred.");
+            map = new google.maps.Map(document.getElementById("map"), {
+            center: {lat: 43.0199770, lng: -83.6876530},
+            zoom: 15
+            });
+		getMapLocError(43.0199770, -83.6876530);
+            break;
+    }
 }
 function getMapLoc(position){
 	$.ajax({
@@ -63,8 +103,54 @@ function getMapLoc(position){
 	}
 });
 }
+function getMapLocError(lat, lng){
+	$.ajax({
+	type: "POST",
+	url: "https://smartparkproject.tk/api/lot/search",
+	dataType: "json",
+	contentType:"application/json; charset=utf-8",
+	data: JSON.stringify({lat: lat.toString(), lng: lng.toString(), distance: 100}),
+	success: function (data) {
+		//TODO: Create pins dynamically based on server results(Done)
+		//var blah = JSON.parse(data.result[0].lat) <-this is structure!
+		var arrayLength = data.result.length;
+		//console.log(arrayLength);
+		var mapOptions = {center: {lat: lat, lng: lng}, zoom: 15};
+		var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+		for(var i = 0; i < arrayLength; i++)
+		{
+		var myLatLng = {lat: JSON.parse(data.result[i].lat), lng: JSON.parse(data.result[i].lng)};
+		let marker = new google.maps.Marker({
+                position: myLatLng,
+		animation: google.maps.Animation.DROP,
+                title: ((data.result[i].name).toString())
+                });
+		let infowindow = new google.maps.InfoWindow({
+    			content: '<p>'+data.result[i].name+'</p>'+'<a class="btn btn-default" onClick="writeToModal('+data.result[i].id+')">Click to View Map</a>'
+  			});
+			//marker function
+		marker.addListener('click', function(){
+			if (marker.getAnimation() !== null) {
+    				marker.setAnimation(null);
+  			} else {
+    			marker.setAnimation(google.maps.Animation.BOUNCE);
+			infowindow.open(map, marker);
+  			}});
+		marker.setMap(map);
+		//console.log(myLatLng);
+		//console.log("Success");
+		}//end for loop
+		
+	},
+	error: function (data) {
+		console.log(data);
+	}
+});
+}
 function writeToModal(id){//TODO: make this function show a modal from the getcustMap function below.
-	getcustMap(id);
+console.log("blah");	
+getcustMap(id);
 }
 function getcustMap(lotID){
 	//var spotnum = $('#distance').val();
@@ -80,13 +166,9 @@ $.ajax({
 	for(var i=0; i < data2.result.length; i++)
 	{
 		avail_array[i] = data2.result[i];
+		
 	}
-	},
-	error: function(data){
-		console.log("error");
-	}
-});
-$.ajax({
+	$.ajax({
 	type: "GET",
 	url: "https://smartparkproject.tk/api/lot/"+lotID+"/",
 	dataType: "json",
@@ -135,4 +217,10 @@ $.ajax({
 		console.log(data);
 	}
 });
+	},
+	error: function(data){
+		console.log("error");
+	}
+});
+
 }
